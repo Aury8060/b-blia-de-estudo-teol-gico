8d29fa90f0ceed85f50eac0450faea077f7b02a5
Trick8060 <patricksilva8060@gmail.com>
2026-07-28 10:52:27 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 };
 
 // 🔑 CHAVE DA GROQ
-const GROQ_API_KEY = "gsk_JaWieMs7SYzZN98nxH8VWGdyb3FYo1UY18KBykv7urEk7UosCZCVgsk_JaWieMs7SYzZN98nxH8VWGdyb3FYo1UY18KBykv7urEk7UosCZCV"; 
+const GROQ_API_KEY = "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"; 
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app); // O getAuth foi removido, agora usamos apenas o banco de dados
